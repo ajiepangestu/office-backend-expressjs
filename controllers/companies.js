da374908a3efca2d58374574bb16ecd775333d6d
Ajie Pangestu <paangestu@gmail.com>
2019-12-30 06:30:04 +0700

    add base CRUD to controller

---
+++ controllers/companies.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.createCompany = (req, res) => {
+
+}
+
+exports.getCompany = (req, res) => {
+
+}
+
+exports.getCompanyById = (req, res) => {
+
+}
+
+exports.updateCompany = (req, res) => {
+
+}
+
+exports.deleteCompany = (req, res) => {
+
+}
---
+++ controllers/workingdays.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.createWorkingDay = (req, res) => {
+
+}
+
+exports.getWorkingDays = (req, res) => {
+
+}
+
+exports.getWorkingDayById = (req, res) => {
+
+}
+
+exports.updateWorkingDay = (req, res) => {
+
+}
+
+exports.deleteWorkingDay = (req, res) => {
+
+}
